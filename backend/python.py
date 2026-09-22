34a53c9922ca9582f609bde567d7e5940035424f
jluu27 <jasonluu123456@gmail.com>
2026-09-22 17:49:34 -0400

    big update

---
+++ backend/python.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)
+@app.route("/")
+def login():
+    return render_template("login.html")
+def home():
+    return render_template('main.html')
+@app.route("/login", methods=["POST"])
+def check_login():
+    username = request.form("username")
+    password = request.form("password")
+    if username == "man27" and password == "password123":
+        return redirect("/main")
+    else:
+        return "incorrect user or pass"
+
+@app.route("/main")
+def main():
+    return render_template("main.html")
+
+app.run(debug=True)
+    
